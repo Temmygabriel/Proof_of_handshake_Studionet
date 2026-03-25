0f76c66d6cb08ca0493579469f337d99516892b2
Temitope Olatunji <68011822+Temmygabriel@users.noreply.github.com>
2026-03-25 12:10:13 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Allows genlayer-js to work properly in Next.js
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
